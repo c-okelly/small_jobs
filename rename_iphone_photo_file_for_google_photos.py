38c1e9ab10710674d16475a2bd193aa2d2f18425
cokelly <okellyconor@gmail.com>
2016-04-07 19:06:19 +0100

    Finished rename_iphonne script. Now renamse correctly by takign command line input with safty feature and error catching

--- rename_iphone_photo_file_for_google_photos.py
+++ rename_iphone_photo_file_for_google_photos.py
@@ -1,25 +1,48 @@
-import os
+import os, sys, getopt
 
-def rename_file():
+"""
+The program will take the location of a directroy from command line. It will then change all of the
+"""
+
+def rename_file(directory):
     items_current_directory = []
 
-    items_current_directory_dirty = os.listdir()
 
-    # Remove hidden files
-    for i in items_current_directory_dirty:
-        if i[0] != ".":
-            items_current_directory.append(i)
+    # Confrim that user wants to proced
+    message = "Please enter y to proceed with change all files in directory " + directory + "\n"
+    user_in = input(message)
 
-    print(items_current_directory)
 
-    for i in items_current_directory:
-        os.renames(i,clean_name(i))
+    if user_in == "y":
+        items_current_directory_dirty = os.listdir(directory)
+        # Remove hidden files
+        for i in items_current_directory_dirty:
+            if i[0] != ".":
+                items_current_directory.append(i)
+
+        print(items_current_directory)
+
+        for i in items_current_directory:
+            new_name = clean_name(i)
+            current_locaiton = directory + "/" + i
+            new_location = directory + "/" +new_name
+            os.renames(current_locaiton,new_location)
+    else:
+        print("Now exiting")
 
 def clean_name(name):
-    name = name.replace("png.png",".png")
-    name = name.replace("mp4.mp4",".mp4")
+    name = name.replace("-","")
+    name = name.replace(".","")
+    name = name.replace("jpg",".jpg")
+    name = name.replace(" ", "_")
 
     return name
 
 if __name__ == '__main__':
-   rename_file()
+    try:
+        command_line_directory_input = sys.argv[1]
+        rename_file(command_line_directory_input)
+    except IndexError:
+        print("No directory was given")
+    except FileNotFoundError:
+        print("The directory could not be found")
