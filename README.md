018d94f7b15483a908656ce0bbc828f5875e6181
cokelly <okellyconor@gmail.com>
2016-05-27 11:44:14 -0500

    Added new gitignore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # small_jobs
+
+This repository contains a number of small scripts that I have written. Most are too small small to warrent their own reops.
+
+Each program should be clearly identified by its titile.
+
+Any description / comments will be included in the program itself.
+
--- rename_iphone_photo_file_for_google_photos.py
+++ rename_iphone_photo_file_for_google_photos.py
@@ -1,3 +1,42 @@
+<<<<<<< HEAD
+import os, sys, getopt
+
+"""
+The program will take the location of a directroy from command line. It will then change all of the
+"""
+
+def rename_file(directory):
+    items_current_directory = []
+
+
+    # Confrim that user wants to proced
+    message = "Please enter y to proceed with change all files in directory " + directory + "\n"
+    user_in = input(message)
+
+
+    if user_in == "y":
+        items_current_directory_dirty = os.listdir(directory)
+        # Remove hidden files
+        for i in items_current_directory_dirty:
+            if i[0] != ".":
+                items_current_directory.append(i)
+
+        print(items_current_directory)
+
+        for i in items_current_directory:
+            new_name = clean_name(i)
+            current_locaiton = directory + "/" + i
+            new_location = directory + "/" +new_name
+            os.renames(current_locaiton,new_location)
+    else:
+        print("Now exiting")
+
+def clean_name(name):
+    name = name.replace("-","")
+    name = name.replace(".","")
+    name = name.replace("jpg",".jpg")
+    name = name.replace(" ", "_")
+=======
 import os
 
 def rename_file():
@@ -18,8 +57,19 @@ def rename_file():
 def clean_name(name):
     name = name.replace("png.png",".png")
     name = name.replace("mp4.mp4",".mp4")
+>>>>>>> 6f18dcda0697a5d265c33c1359dd85941e33804f
 
     return name
 
 if __name__ == '__main__':
+<<<<<<< HEAD
+    try:
+        command_line_directory_input = sys.argv[1]
+        rename_file(command_line_directory_input)
+    except IndexError:
+        print("No directory was given")
+    except FileNotFoundError:
+        print("The directory could not be found")
+=======
    rename_file()
+>>>>>>> 6f18dcda0697a5d265c33c1359dd85941e33804f
