80c1e86be4b013139fc07973173db258468cd2ad
cokelly <okellyconor@gmail.com>
2016-10-29 14:32:58 +0100

    Fixed merge conflicts and deleated redundant file

--- rename_iphone_photo_file_for_google_photos.py
+++ rename_iphone_photo_file_for_google_photos.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os, sys, getopt
 
 """
@@ -36,33 +35,10 @@ def clean_name(name):
     name = name.replace(".","")
     name = name.replace("jpg",".jpg")
     name = name.replace(" ", "_")
-=======
-import os
-
-def rename_file():
-    items_current_directory = []
-
-    items_current_directory_dirty = os.listdir()
-
-    # Remove hidden files
-    for i in items_current_directory_dirty:
-        if i[0] != ".":
-            items_current_directory.append(i)
-
-    print(items_current_directory)
-
-    for i in items_current_directory:
-        os.renames(i,clean_name(i))
-
-def clean_name(name):
-    name = name.replace("png.png",".png")
-    name = name.replace("mp4.mp4",".mp4")
->>>>>>> 6f18dcda0697a5d265c33c1359dd85941e33804f
 
     return name
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     try:
         command_line_directory_input = sys.argv[1]
         rename_file(command_line_directory_input)
@@ -70,6 +46,3 @@ if __name__ == '__main__':
         print("No directory was given")
     except FileNotFoundError:
         print("The directory could not be found")
-=======
-   rename_file()
->>>>>>> 6f18dcda0697a5d265c33c1359dd85941e33804f
